f0a0e9ff47eea33390193a86f50f7027fd37c147
Ethan Swafford <ethanswafford@yahoo.com>
2022-10-29 00:52:54 -0500

    devPush

--- code.js
+++ code.js
@@ -1,1 +1,22 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily',
+    'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.',
+    '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'
+];
+
+
+
+// Use an array method to remove the last string of the array secretMessage.
+
+
+secretMessage.pop();
+console.log(secretMessage);
+
+// Great! You can check your work by logging the .length of the array.
+// At this point, the length should be 1 less than the original length.
+
+console.log(secretMessage.length);
+
+
+
+
 console.log('CONNECTED');
